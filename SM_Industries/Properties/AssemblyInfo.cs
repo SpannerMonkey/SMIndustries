4f801dae8b9042721b783acb318e01447806a4ba
SpannerMonkey <v8tony@rocketmail.com>
2017-10-15 19:39:58 +0100

    Housekeeping
    exclude from project unused as yet  CS introduce file structure

--- SM_Industries/Properties/AssemblyInfo.cs
+++ SM_Industries/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SMIndustries for ksp 1.3.1")]
+[assembly: AssemblyTitle("SMIndustries for ksp 1.3.1  refact test")]
 [assembly: AssemblyDescription("Adding Propellor spinner and extras to SMI suite")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.94")]
-[assembly: AssemblyFileVersion("0.1.0.94")]
+[assembly: AssemblyVersion("0.1.0.95")]
+[assembly: AssemblyFileVersion("0.1.0.95")]
